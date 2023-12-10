bf67569823a88a3a23dce8f8c3b1d4239660b026
kelvin ward <canigetyournumber101010@gmail.com>
2023-12-10 08:35:21 +0700

    updated .env

--- app/database.py
+++ app/database.py
@@ -4,9 +4,10 @@ from sqlalchemy.orm import sessionmaker
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
+from .config import settings
 
 
-SQLALCHEMY_DATABASE_URL = 'postgresql+psycopg2://postgres:kelvin@localhost/fastapi'
+SQLALCHEMY_DATABASE_URL = f'postgresql+psycopg2://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
--- app/oauth2.py
+++ app/oauth2.py
@@ -5,6 +5,7 @@ from fastapi import Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from typing import Annotated
 from sqlalchemy.orm import Session
+from .config import settings
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/login')
 
@@ -13,9 +14,9 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/login')
 #Expriation time
 
 
-SECRET_KEY = "kelvinward1010"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
+SECRET_KEY = settings.secret_key
+ALGORITHM = settings.algorithm
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
 
 
 def create_access_token(data: dict):
